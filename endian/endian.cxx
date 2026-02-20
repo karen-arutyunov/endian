39ce1a5eb26634f8182fe84c138badd143e3cf95
Karen Arutyunov <karen@codesynthesis.com>
2026-02-20 16:04:48 +0200

    Test <sys/param.h> header

--- endian/endian.cxx
+++ endian/endian.cxx
@@ -1,13 +1,16 @@
-#include <sys/endian.h>
+#include <sys/param.h>
 
 //
 // <endian.h> - POSIX header which, in particular, defines BYTE_ORDER,
 //              BIG_ENDIAN, and LITTLE_ENDIAN. Available on Linux, FreeBSD,
-//              but not on MacOS.
+//              but not MacOS.
 //
 // <machine/endian.h> - not a POSIX header. Typically available on BSD-derived
 //                      systems, in particular, on FreeBSD and MacOS.
-
+//
+// <sys/endian.h>     - not a POSIX header. Typically available on BSD-derived
+//                      systems, in particular, on FreeBSD but not MacOS.
+//
 int main ()
 {
 #if !defined(BYTE_ORDER) || !defined(BIG_ENDIAN) || !defined(LITTLE_ENDIAN)
